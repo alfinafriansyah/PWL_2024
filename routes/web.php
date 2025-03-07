9c3b6a52da2e5771c03ed4942af5f0382f5d66e6
afriansyah <alfinafriansyah68@gmail.com>
2025-03-07 21:35:18 +0700

    Jobsheet 2 - Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,21 +23,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-return 'Hello World';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
 
 Route::get('/world', function () {
 return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return 'NIM: 2341760089 Nama: Alfin Afriansyah';
-});
+// Route::get('/about', function () {
+//     return 'NIM: 2341760089 Nama: Alfin Afriansyah';
+// });
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
@@ -42,10 +50,20 @@ Route::get('/posts/{post}/comments/{comment}', function
 return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function
-($articlesId) {
-return 'Halaman Artikel dengan ID '.$articlesId;
-});
+// Route::get('/articles/{id}', function
+// ($articlesId) {
+// return 'Halaman Artikel dengan ID '.$articlesId;
+// });
+
+// Praktikum 2 Langkah 6
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Praktikum 2 Langkah 7
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 Route::get('/user/{name?}', function ($name=null) {
     return 'Nama saya '.$name;
@@ -54,3 +72,5 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
+
+Route::resource('photos', PhotoController::class); 
